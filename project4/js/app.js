3ceeb6add097d9a4a39fbfa4b7df540e4715fef8
Quang Nguyen <quangnguyenos0302@gmail.com>
2020-11-19 22:04:29 +0700

    project 4

---
+++ project4/js/app.js
@@ -0,0 +1,26 @@
+
+(function() {
+//Select the input element
+const form = document.querySelector('#message-form')
+//Set up Submit Button
+form.addEventListener('submit', function(e){
+    // prevent the form's default submission action
+    e.preventDefault()
+    //Get user's input from from
+    const message = document.querySelector('#message')
+    const feedback = document.querySelector('.feedback')
+    const messageContent = document.querySelector('.message-content')
+
+    if (message.value === '') {
+        feedback.classList.add('show');
+        setTimeout(function(){
+            feedback.classList.remove('show');
+        }, 2000);
+    } else {
+        messageContent.innerHTML = message.value;
+    }
+})
+})()
+
+
+
