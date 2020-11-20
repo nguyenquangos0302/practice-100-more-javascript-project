0692711fca51b436059d5377a53d2e9244bdbb22
QuangNguyen <quangntngithub@gmail.com>
2020-11-20 08:10:03 +0700

    project5-1

--- project5/js/app.js
+++ project5/js/app.js
@@ -1,18 +1,22 @@
-(function(){
-  const buttons = document.querySelectorAll('.counterBtn')
-  let count= 0
+(function () {
+  const buttons = document.querySelectorAll('.counterBtn');
+  const counter = document.getElementById('counter');
+  let count = 0;
 
-  //Add event listeners and functionailty to each button  
-  buttons.forEach(function(button) {
-    button.addEventListener('click', function(event) {
-      if (button.className === 'prevBtn') {
+  //Add event listeners and functionailty to each button
+  buttons.forEach(function (button) {
+    button.addEventListener('click', function (event) {
+      if (button.classList.contains('prevBtn')) {
         count--;
       } else {
         count++;
       }
-
-      
-
-    })
-  })
-})()
+      counter.innerHTML = count;
+      if (count < 0) {
+        counter.style.color = 'red';
+      } else if (count > 0) {
+        counter.style.color = 'green';
+      }
+    });
+  });
+})();
